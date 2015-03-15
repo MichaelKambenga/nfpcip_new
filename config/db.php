8846176cb3e495253247e3ede4082a92454641f5
Michael <Michael>
2015-03-15 17:47:47 +0300

    Initial Commit ti the NFPCIP

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=nfpcip_new',
+    'username' => 'root',
+    'password' => 'Michael',
+    'charset' => 'utf8',
+];
